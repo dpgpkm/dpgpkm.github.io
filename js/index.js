f74642632959de0982c03a6bf85125897f0d2ebc
0x24a <tanhanze@qq.com>
2024-08-03 17:48:46 +0800

    Change main page

---
+++ js/index.js
@@ -0,0 +1,3 @@
+document.querySelector("#javascript-loading-stats").setAttribute("description", "Ready")
+document.querySelector("#openpgp-loading-stats").setAttribute("description", `Ready, ${openpgp.config.versionString}`)
+console.warn("Warning: DO NOT PASTE ANYTHING HERE IF YOU DON'T KNOW WHAT IS THIS.")
